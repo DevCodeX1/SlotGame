dbaed35a61b7f46254ddfb2f2ea7c35173e23bff
DevCodeX <countryboya20@gmail.com>
2023-09-11 13:54:04 -0500

    reels function update2

--- project.js
+++ project.js
@@ -78,16 +78,23 @@ const spin = () =>{
     }
 
   }
-  const reels = [[], [], []];
+
+  const reels = [];
   for(let i = 0; i < COLS; i++){
+    reels.push([])
     const reelSymbols = [...symbols];
     for (let j = 0; j < ROWS; j++){
         const randomIndex = Math.floor(Math.random() * reelSymbols.length);
-        const selectedSymbol = reelSymbols[randomIndex];
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
     }
   }
+
+  return reels;
 };
-spin()
+const reels = spin();
+console.log(reels)
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
